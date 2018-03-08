912d87f4b1154de36a68257ae006704df6d42906
your father <554737215@qq.com>
2018-03-08 16:00:23 +0800

    contact

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,12 @@
 
+(function () {
+    $(`footer button`).click(function () {
+        if(!$(`footer input`).val() || !$(`footer textarea`).val()){
+            let html = ``;
+            html +=`<p class="warning">↑这些是必填项<p> `
+            $(`footer input:odd`).addClass("live").after(html)
+            $(`footer textarea`).addClass("live").after(html)
+
+        }
+    })
+})()
