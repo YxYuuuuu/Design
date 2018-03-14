0c8ca6e01a204b0a8e8e8a2e9a4f0f0f0ef4fdcb
your father <554737215@qq.com>
2018-03-14 18:20:40 +0800

    team.html&修改

--- src/javascript/design.js
+++ src/javascript/design.js
@@ -10,26 +10,25 @@
         background(i)
     };
 
-    $(`.showBox`).addClass("done");
     $(`li`).eq(0).addClass("click");
     function load(obj) {
         for (var i = 0,len = $(`.showBox`).length;i<len;i++){
-           $(`.wrap`).eq(obj).children(".showBox").eq(i).addClass("active").removeClass("done")
-            $(`.wrap`).eq(obj).css({display:"block"}).siblings("div").css({display:"none"}).children(".showBox").eq(i).addClass("done").removeClass("active")
+           $(`.wrap`).eq(obj).children(".showBox").eq(i).addClass("active");
+           // $(`.showBox`).removeAttr("style")
+            $(`.wrap`).eq(obj).css({display:"block"}).siblings("div").css({display:"none"}).children(".showBox").addClass("done")
         }
     }
-    $(`li`).click(function () {
+
+    $(`li`).on("click",function () {
         $(this).addClass("click").siblings().removeClass("click")
+        let idx = $(this).index();
+        load(idx);
+
     });
     $();
-    $(`li`).click(function () {
-        let idx = $(this).index();
-
-         load(idx)
-        })
-
     window.onload = function () {
 
-       load(0)
+        load(0)
     };
+
 })()
--- src/javascript/team.js
+++ src/javascript/team.js
@@ -1,10 +1,34 @@
 (()=>{
 
-	let img =document.getElementById('img');
-	img.classList.add("start");
-	window.onload = () =>{
-		img.classList.remove("start")
-		img.classList.add("end");
-		
-	}
+    let xhr = new XMLHttpRequest();
+    xhr.open(`GET`,`json/team.json`,true)
+    xhr.onload = function () {
+        let obj = JSON.parse(xhr.response),
+            html = ``;
+        for(var i = 0,len = obj[`member`].length;i<len;i++){
+            html +=`
+            <div class="wrap">
+             <div class="msg">
+                <img src="${obj[`member`][i][`img`]}" alt="">
+                <p>${obj[`member`][i][`status`]}</p>
+                <p>${obj[`member`][i][`position`]}${obj[`member`][i][`name`]}</p>
+               
+                        </div>
+            <div class="status">
+                <div class="hr"></div>
+                <span class="text">${obj[`member`][i][`resume`]}</span>
+            </div>
+                        
+                </div>
+                               
+`
+        }
+       $(`#team`).html(html);
+        $(`.wrap`).click(function () {
+            window.location.href = `http://5a1d369c61574.t73.qifeiye.com/?post_type=products&page_id=12870`
+        })
+    }
+    xhr.send(null)
+
+
 })()
